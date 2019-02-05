a7cfe6e4ebb0b9603ff3540d9cef42bc0b0dac27
Ezgi Seyis <ezgicay_1993@hotmail.com>
2019-02-05 17:26:27 +0100

    request controller show with authorize added

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,9 +1,14 @@
 class RequestsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
     @requests = Request.all
   end
 
   def show
-    @request = Request.find(params[:id])
+    authorize @request
+    if user_signed_in?
+      @request = Request.find(params[:id])
+    end
   end
 end

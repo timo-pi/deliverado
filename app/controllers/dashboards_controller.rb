719975d05e21ebd0b1ea0383d3ac9b5139214f11
Christian Uhmann <uhmann@outlook.de>
2019-02-05 17:01:39 +0100

    Dashboard created and enum in Model enabled

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,8 +1,8 @@
 class DashboardsController < ApplicationController
-  # skip_after_action :verify_authorized, only: :index
-  # skip_after_action :verify_policy_scoped, only: :index
+  skip_after_action :verify_authorized, only: :index
+  skip_after_action :verify_policy_scoped, only: :index
 
   def index
-    @my_requests = Request.all
+    @my_deliveries = Delivery.where(user_id: current_user.id)
   end
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,4 @@
 class Request < ApplicationRecord
-  # has_many :deliveries ??
+  has_one :delivery
   validates :order_number, :delivery_address, presence: true
 end

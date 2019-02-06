77a338c7f54dbb499c7378fd4871cc8d0c7df418
Christian Uhmann <uhmann@outlook.de>
2019-02-06 12:21:57 +0100

    Geocoding enabled

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,12 @@
 class Request < ApplicationRecord
-  has_one :delivery
+  has_one :delivery, dependent: :destroy
   validates :order_number, :delivery_address, presence: true
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_delivery_address?
+
+  private
+
+  def address
+    [delivery_address, city].compact.join(', ')
+  end
 end
--- app/models/route.rb
+++ app/models/route.rb
@@ -1,4 +1,26 @@
 class Route < ApplicationRecord
   belongs_to :user
   validates :start_address, :end_address, :delivery_radius, presence: true
+  geocoded_by :start_address, latitude: :start_latitude, longitude: :start_longitude
+  geocoded_by :end_address, latitude: :end_latitude, longitude: :end_longitude
+  before_save :geocode_endpoints
+
+  private
+
+  def geocode_endpoints
+    if start_address_changed?
+      geocoded = Geocoder.search(start_address).first
+      if geocoded
+        self.start_latitude = geocoded.latitude
+        self.start_longitude = geocoded.longitude
+      end
+    end
+    if end_address_changed?
+      geocoded = Geocoder.search(end_address).first
+      if geocoded
+        self.end_latitude = geocoded.latitude
+        self.end_longitude = geocoded.longitude
+      end
+    end
+  end
 end

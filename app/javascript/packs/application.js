de929649976db61eff9d66be8684da4fca412985
khaled Al-Ammari <khaled99@live.de>
2019-02-07 16:46:24 +0100

    Slider angepasst

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,7 +5,8 @@ import { initMapbox } from '../plugins/init_mapbox';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
 import { initAutocomplete } from '../plugins/init_autocomplete';
+import { init_slider } from "../plugins/slider.js";
 
 initMapbox();
 initAutocomplete();
-
+init_slider("#route_radius");

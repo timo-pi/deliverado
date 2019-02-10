f58e693e2577e8d0bb54bbfacdb87f0769d9425d
Timo Piechotta <piechotta@mediamarktsaturn.com>
2019-02-10 17:02:36 +0100

    Route show better styling...

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -3,7 +3,7 @@ class RequestsController < ApplicationController
 
   def index
     # Redirect to step_1 if no route exists -> this slows down the landingpage video loading... why???
-    redirect_to step_1_path unless Route.all.where(user: current_user.id).exists?
+    redirect_to step_1_path unless Route.where(user: current_user.id).exists?
 
     @requests = policy_scope(Request).where(status: 0)
     if params[:query].present? # query = die suche....

a129df449cb5d9d664e1126c1c5e339080968f7a
Christian Uhmann <uhmann@outlook.de>
2019-02-11 14:39:46 +0100

    How it Works added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :howto]
 
   def home
     # render 'new'
+  end
+
+  def howto
   end
 end
